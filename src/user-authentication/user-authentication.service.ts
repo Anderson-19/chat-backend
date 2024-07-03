d9908a7ccd7de85815ee0b05a66ea9edbe180e11
Anderson-19 <andersonpantor@gmail.com>
2024-07-03 15:41:51 -0400

    Método para verificar si el token es válido

--- src/user-authentication/user-authentication.service.ts
+++ src/user-authentication/user-authentication.service.ts
@@ -9,14 +9,20 @@ import * as bcryptjs from 'bcryptjs';
 import { Model } from 'mongoose';
 import { JwtService } from '@nestjs/jwt';
 
-import { LoginUserDto, RegisterUserDto, UpdateUserAuthenticationDto } from './dto';
+import {
+  LoginUserDto,
+  RegisterUserDto,
+  UpdateUserAuthenticationDto,
+} from './dto';
 import { User } from './entities/user-authentication.entity';
 import { JwtPayload, LoginResponse } from './interfaces';
 
 @Injectable()
 export class UserAuthenticationService {
-
-  constructor(@InjectModel(User.name) private userModel: Model<User>, private jwtService: JwtService) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
 
   async register(registerDto: RegisterUserDto): Promise<LoginResponse> {
     try {
@@ -24,8 +30,9 @@ export class UserAuthenticationService {
 
       const userExist = await this.userModel.findOne({ email: userData.email });
 
-      if (userExist) throw new UnauthorizedException(`${ userData.email } already exists!`);
-  
+      if (userExist)
+        throw new UnauthorizedException(`${userData.email} already exists!`);
+
       const newUser = new this.userModel({
         password: bcryptjs.hashSync(password, 10),
         ...userData,
@@ -34,48 +41,71 @@ export class UserAuthenticationService {
       await newUser.save();
       const { password: _, ...user } = newUser.toJSON();
 
-      return {user};
+      return { user };
     } catch ({ response: _, ...error }) {
       if (error.status === 401) throw new BadRequestException(error);
-      
+
       throw new InternalServerErrorException(error);
     }
   }
 
-  async login( loginDto: LoginUserDto ):Promise<LoginResponse> {
+  async login(loginDto: LoginUserDto): Promise<LoginResponse> {
     try {
-      
       const { email, password } = loginDto;
-  
+
       const user = await this.userModel.findOne({ email });
-      if ( !user ) throw new UnauthorizedException(`${ email } does not exist!`);
-      
-      if ( !bcryptjs.compareSync( password, user.password ) ) {
-        throw new UnauthorizedException(`${ password } does not exist!`);
+      if (!user) throw new UnauthorizedException(`${email} does not exist!`);
+
+      if (!bcryptjs.compareSync(password, user.password)) {
+        throw new UnauthorizedException(`${password} does not exist!`);
       }
-  
-      const { password:_, ...rest  } = user.toJSON();
+
+      const { password: _, ...rest } = user.toJSON();
 
       return {
         user: rest,
         token: this.getJwtToken({ id: user.id }),
-      }
-    
+      };
     } catch ({ response: _, ...error }) {
-      if (error.status === 401) throw new BadRequestException(error);
-      
+      if (error.status === 401) throw new UnauthorizedException(error);
+
       throw new InternalServerErrorException(error);
     }
-
   }
 
-  getJwtToken( payload: JwtPayload ) {
+  getJwtToken(payload: JwtPayload) {
     const token = this.jwtService.sign(payload);
     return token;
   }
 
-  findAll() {
-    return `This action returns all userAuthentication`;
+  async checkToken(token: string) {
+    const verifyToken: JwtPayload = this.jwtService.verify(token, {
+      secret: process.env.JWT_SEED,
+    });
+
+    try {
+      if (!verifyToken)
+        throw new UnauthorizedException(`Token does not exist!`);
+
+      const user = await this.userModel.findOne({ _id: verifyToken.id });
+
+      if (!user)
+        throw new UnauthorizedException(
+          `The User: ${verifyToken.id} does not exist!`,
+        );
+
+      const { password: _, ...rest } = user.toJSON();
+
+      return {
+        user: rest,
+        token,
+      };
+    } catch (error) {
+      if (error.status === 401) throw new UnauthorizedException(error);
+
+      throw new InternalServerErrorException(error);
+
+    }
   }
 
   findOne(id: number) {
