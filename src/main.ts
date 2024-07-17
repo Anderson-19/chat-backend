65088e3b7b6ff989d717088dd6184fa0fedeba3a
Anderson-19 <andersonpantor@gmail.com>
2024-07-17 15:56:12 -0400

    Servidor de archivos estáticos

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  
   
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,10 +14,11 @@ async function bootstrap() {
     }),
   );
 
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
 
   app.enableCors();
-
-
   await app.listen(3000);
 }
 bootstrap();
