b47aea16c9cb7665ab690bff436920efb5b12882
Anderson-19 <andersonpantor@gmail.com>
2024-07-17 15:57:41 -0400

    Importación de los módulos creados

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,19 @@ import { Module } from '@nestjs/common';
 import { UserAuthenticationModule } from './user-authentication/user-authentication.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { JwtService } from '@nestjs/jwt';
+import { ConversationModule } from './conversation/conversation.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
-    UserAuthenticationModule
+    UserAuthenticationModule,
+    ConversationModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
